7b57bdbd81e2d533dd055dd0f82041e9540847d8
Serhii Makarenko <s.makarenko2003@gmail.com>
2025-01-18 14:29:56 +0200

    fix gourmand-choice carousel

--- js/swiper.js
+++ js/swiper.js
@@ -20,6 +20,30 @@ new Swiper('.testimonials__carousel-wrapper', {
             slidesPerView: 1
         }
     },
-
   });
 
+  new Swiper('.choice-carousel-container', {
+    // Optional parameters
+    loop: true,
+    // slidesPerView: 'auto', // Автоматично підлаштовує ширину під слайд
+    // centeredSlides: true, // Центрує активний слайд
+    // spaceBetween: 500, // Без проміжків між слайдами
+    // If we need pagination
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        990: {
+            slidesPerView: 3
+        }
+    },
+  });
